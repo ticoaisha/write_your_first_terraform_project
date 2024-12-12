b7620bb118cf1907798b9999f15e375c6393fa18
Aishe <ticoaisha@gmail.com>
2024-12-11 22:19:28 -0500

    main.tf updated for s3 configuration

--- aws/remote_state/main.tf
+++ aws/remote_state/main.tf
@@ -16,7 +16,9 @@ terraform {
 # CONFIGURE OUR AWS CONNECTION
 # ------------------------------------------------------------------------------
 
-provider "aws" {}
+provider "aws" {
+  region  = "us-east-1"
+}
 
 # ------------------------------------------------------------------------------
 # CREATE THE S3 BUCKET
@@ -31,19 +33,27 @@ locals {
 resource "aws_s3_bucket" "terraform_state" {
   # With account id, this S3 bucket names can be *globally* unique.
   bucket = "${local.account_id}-terraform-states"
+}
 
-  # Enable versioning so we can see the full revision history of our
-  # state files
-  versioning {
-    enabled = true
+# Create a new resource specifically for versioning
+# so we can see the full revision history of our state files
+
+resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
   }
+}
 
-  # Enable server-side encryption by default
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+# Create a new resource specifically for server-side encryption
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_encryption" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
   }
 }
